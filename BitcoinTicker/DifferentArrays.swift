9d01369768a9c71c4f0ff4374843f4ae9ef213c2
Michael Chapman <submariner100@gmail.com>
2019-04-04 12:26:12 +0100

    Final commit

---
+++ BitcoinTicker/DifferentArrays.swift
@@ -0,0 +1,18 @@
+//
+//  DifferentArrays.swift
+//  BitcoinTicker
+//
+//  Created by Macbook on 04/04/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+// currencys
+
+let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+let currencySymbolArray = ["$","R$","$","¥","€","₤","$","Rp","₪","₹","¥","$","Kr","$","z","lei","₽","kr","$","$","R"
+]
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '9.0'
+
+target 'BitcoinTicker' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'SwiftyJSON', '~> 4.0'
+  pod 'Alamofire'
+	pod 'SVProgressHUD'
+
+end
